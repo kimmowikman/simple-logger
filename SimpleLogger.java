fcb89eda59ee7be67010fcbc77c0578b69fb377d
Kimmo Wikman <kimmo.wikman@luotocompany.fi>
2021-12-16 07:38:29 +0200

    Create SimpleLogger.java
    First version of SimpleLogger.

---
+++ SimpleLogger.java
@@ -0,0 +1,36 @@
+package fi.luotocompany.logger;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+/**
+ * Simple and secure logger by Kimmo Wikman, Luoto Company
+ */
+public class SimpleLogger {
+
+  public enum LogLevel { DEBUG, INFO, WARN, ERROR }
+
+  private static final DateTimeFormatter DATE_FORMAT = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss.SSS");
+
+  private final LogLevel level;
+  private final String sourceName;
+
+  public SimpleLogger(final LogLevel level, final Class source) {
+    this.level = level;
+    this.sourceName = source.getName();
+  }
+
+  public void log(final LogLevel logLevel, final String message) {
+    if (this.level.ordinal() <= logLevel.ordinal()) {
+      final StringBuilder sb = new StringBuilder();
+      sb.append(LocalDateTime.now().format(DATE_FORMAT))
+          .append(" [")
+          .append(logLevel.toString())
+          .append("] ")
+          .append(sourceName)
+          .append(" - ")
+          .append(message);
+      System.out.println(sb.toString());
+    }
+  }
+}
